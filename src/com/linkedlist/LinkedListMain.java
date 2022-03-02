6adcc4caf64db34f429e0de8d45be54611af61a2
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-03-02 21:36:59 +0530

    add data values uc2 is done

--- src/com/linkedlist/LinkedListMain.java
+++ src/com/linkedlist/LinkedListMain.java
@@ -1,20 +1,17 @@
 package com.linkedlist;
 
+import java.util.Scanner;
+
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome to DataStructure Program On LinkedList ");
-        Node firstNode = new Node(56);
-        Node secondNode = new Node(30);
-        Node thirdNode = new Node(70);
-        Node head = firstNode;
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-        Node tail = thirdNode;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to add the data at the start");
 
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.data);
-            temp = temp.next;
+        switch (scanner.nextInt()) {
+            case 1:
+                Operations.addeElementAtStart();
         }
+
     }
 }
