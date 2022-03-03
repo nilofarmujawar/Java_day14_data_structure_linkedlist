0cbafb093571c64c2193fa9f01534569b5b262cc
nilofarmujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-03-03 15:44:44 +0530

    Added : Create a Linked List By Adding 30 and 56 to 70

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -1,32 +1,99 @@
 package com.linkedlist;
 
-
+/*
+ *creating a class name as LinkedList
+ *
+ * in this class we perform all th operations
+ */
 public class LinkedList {
     Node head;
-    Node tail;
 
-    public void push(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            Node temp = head;
-            this.head = newNode;
-            newNode.next = temp;
+    // create node class
+
+    /*
+     * class Node that hold data and a reference/link
+	 * to the next Node in the list
+     */
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
+    /*
+     * Method to add a node at the beginning of the list
+     */
 
-    public void print() {
+    public Node insert(int data) {
+
+        /*
+         * Create a new node
+         */
+        Node newNode = new Node(data);
+
+        /*
+         * Check if the list is empty
+         */
         if (head == null) {
-            System.out.println("LinkedList is Empty");
+
+            /*
+             * Make the new node as head
+             */
+            head = newNode;
+
         } else {
             Node temp = head;
-            while (temp != null) {
-                System.out.print(temp.data + " ");
+
+            /*
+             * traverse to the end of the list
+             */
+            while (temp.next != null) {
                 temp = temp.next;
             }
+            temp.next = newNode;
+        }
+        return newNode;
+    }
+
+    /*
+     * create a print method
+     */
+    public void print() {
+
+        /*
+         * head store in temp variable
+         */
+        Node temp = head;
+
+        /*
+         * While loop using
+         *
+         * If temp is not equal to null then print in sout data
+         */
+        while (temp != null) {
+            System.out.print(temp.data + "-> ");
+            temp = temp.next;
+        }
+
+    }
+         /*
+          * create a push method for adding new node and passing parameter
+          */
+        public void push ( int data){
+            /*
+             * Create a new node
+             */
+            Node newNode = new Node(data);
+            /*
+             * new node is head
+             */
+            newNode.next = head;
+            head = newNode;
 
         }
-    }
+
 }
+
